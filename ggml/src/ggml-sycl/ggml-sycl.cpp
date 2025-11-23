479ba5ec6b09efcf3b6560d2953a450613e0dca8
Gie "Max" Vanommeslaeghe <gievanom@hotmail.com>
2025-11-23 23:26:17 +0100

    Set reorder optimization to false for Intel GPUs
    Disable reorder optimization for Intel GPU performance.

--- ggml/src/ggml-sycl/ggml-sycl.cpp
+++ ggml/src/ggml-sycl/ggml-sycl.cpp
@@ -94,7 +94,7 @@ static ggml_sycl_device_info ggml_sycl_init() {
         info.devices[i].cc =
             100 * prop.get_major_version() + 10 * prop.get_minor_version();
         info.devices[i].nsm = prop.get_max_compute_units();
-        info.devices[i].opt_feature.reorder = device.ext_oneapi_architecture_is(syclex::arch_category::intel_gpu);
+        info.devices[i].opt_feature.reorder = false;  // Reorder optimization hurts Intel GPU performance
         info.devices[i].smpbo = prop.get_local_mem_size();
 
         info.max_work_group_sizes[i] = prop.get_max_work_group_size();
